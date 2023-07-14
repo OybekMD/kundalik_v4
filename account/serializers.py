d6b5a365b2984804d060317ea89620ead291007a
oybekmd <oybekatamatov999@gmail.com>
2023-07-14 09:09:37 +0500

    asd

--- account/serializers.py
+++ account/serializers.py
@@ -1,22 +1,12 @@
 from rest_framework import serializers
-from .models import PersonModel, StudentModel, TeacherModel
-from django.shortcuts import get_object_or_404
-
-    
-class PersonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PersonModel
-        fields = '__all__'
-
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StudentModel
-        fields = '__all__'
-
+from .models import TeacherModel,StudentModel
 
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = TeacherModel
         fields = '__all__'
-
+        
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StudentModel
+        fields = '__all__'
--- account/views.py
+++ account/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import PersonModel, StudentModel, TeacherModel
-from .serializers import PersonSerializer, StudentSerializer, TeacherSerializer
-from .permissions import IsOwnerPermission
-
-class AllCreateAccountView(generics.ListCreateAPIView):
-    queryset = PersonModel.objects.all()
-    serializer_class = PersonSerializer
-
 # Create your views here.
+from .models import StudentModel,TeacherModel
+from .serializers import StudentSerializer,TeacherSerializer
+
+class TeacherView(generics.ListCreateAPIView):
+    queryset = TeacherModel.objects.all()
+    serializer_class = TeacherSerializer
+
+class TeacherDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TeacherModel.objects.all()
+    serializer_class = TeacherSerializer
+
+class StudentView(generics.ListCreateAPIView):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
